eclipse-kuksa/kuksa-databroker

commit f38363d4ffc4d50d3ce4d5381112b6c84666e92c

Switch to 2021 edition of Rust
Signed-off-by: Sebastian Schildt <sebastian.schildt@de.bosch.com>

databroker-cli/Cargo.toml
@@ -15,7 +15,7 @@
 name = "databroker-cli"
 version = "0.4.1"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [dependencies]

databroker/Cargo.toml
@@ -15,7 +15,7 @@
 name = "databroker"
 version = "0.4.1"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [lib]

Cargo.toml
@@ -13,6 +13,8 @@
 
 [workspace]
 
+resolver = "2"
+
 members = [
     "databroker",
     "databroker-proto",

databroker-proto/Cargo.toml
@@ -15,7 +15,7 @@
 name = "databroker-proto"
 version = "0.4.1"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [dependencies]

lib/common/Cargo.toml
@@ -15,7 +15,7 @@
 name = "kuksa-common"
 version = "0.1.0"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [dependencies]

lib/kuksa/Cargo.toml
@@ -15,7 +15,7 @@
 name = "kuksa"
 version = "0.1.0"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [dependencies]

lib/sdv/Cargo.toml
@@ -15,7 +15,7 @@
 name = "kuksa-sdv"
 version = "0.1.0"
 authors = ["Eclipse KUKSA Project"]
-edition = "2018"
+edition = "2021"
 license = "Apache-2.0"
 
 [dependencies]

commit 2173ae9e07f06fdbc71a1641e3e52ca1cc5b08c5

Fix `--help` in clap
This functionality was broken by "f38363d - Switch to 2021 edition of
Rust", or more specifically with the change to resolver = 2.

The `--help` flag requires a feature in clap to be enabled, which is either a newly introduced requirement, or the feature(s) were somehow included implicitly when using resolver = 1.

databroker-cli/Cargo.toml
@@ -39,6 +39,9 @@ clap = { workspace = true, features = [
     "std",
     "env",
     "derive",
+    "help",
+    "error-context",
+    "usage",
 ] }
 regex = "1.6.0"
 http = "0.2.8"

databroker/Cargo.toml
@@ -45,7 +45,9 @@ tracing-subscriber = { version = "0.3.11", default-features = false, features = 
 clap = { workspace = true, features = [
     "std",
     "env",
-    "derive",
+    "help",
+    "usage",
+    "error-context",
 ] }
 sqlparser = "0.16.0"
 serde = { version = "1.0", features = ["derive"] }

databroker/src/main.rs
@@ -171,8 +171,7 @@ async fn read_metadata_file<'a, 'b>(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let version = option_env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT")
-        .unwrap_or(option_env!("VERGEN_GIT_SHA").unwrap_or("unknown"));
+    let version = option_env!("CARGO_PKG_VERSION").unwrap_or_default();
 
     let about = format!(
         concat!(
@@ -190,7 +189,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         option_env!("VERGEN_CARGO_DEBUG").unwrap_or(""),
     );
 
-    let mut parser = Command::new("Kuksa Data Broker");
+    let mut parser = Command::new("Kuksa Databroker");
     parser = parser
         .version(version)
         .about(about)