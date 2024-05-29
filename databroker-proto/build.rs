3a0ea4d67f20610e6427204aa38df5b417ed78a3
Rafael RL <rafael.ruiz-lucena@de.bosch.com>
2024-05-29 11:16:31 +0200

    Update provide stream response and add files to the build project

--- databroker-proto/build.rs
+++ databroker-proto/build.rs
@@ -23,6 +23,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 "proto/sdv/databroker/v1/collector.proto",
                 "proto/kuksa/val/v1/val.proto",
                 "proto/kuksa/val/v1/types.proto",
+                "proto/kuksa/val/v2/types.proto",
+                "proto/kuksa/val/v2/val.proto"
             ],
             &["proto"],
         )?;
