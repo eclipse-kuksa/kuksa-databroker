7951c62702e3eb3052bcf8f4e2c3235cd5adb6f5
Erik Jaegervall <erik.jaegervall@se.bosch.com>
2024-09-18 14:38:47 +0200

    Fix regressions

--- databroker-cli/src/kuksa_cli.rs
+++ databroker-cli/src/kuksa_cli.rs
@@ -312,7 +312,7 @@ pub async fn kuksa_main(_cli: Cli) -> Result<(), Box<dyn std::error::Error>> {
                                     }
                                 }
                                 Err(err) => {
-                                    cli::print_error(cmd, &format!("Malformed token: {err}"))?
+                                    cli::print_error(cmd, format!("Malformed token: {err}"))?
                                 }
                             }
                         }
@@ -350,12 +350,12 @@ pub async fn kuksa_main(_cli: Cli) -> Result<(), Box<dyn std::error::Error>> {
                                         }
                                     }
                                     Err(err) => {
-                                        cli::print_error(cmd, &format!("Malformed token: {err}"))?
+                                        cli::print_error(cmd, format!("Malformed token: {err}"))?
                                     }
                                 },
                                 Err(err) => cli::print_error(
                                     cmd,
-                                    &format!(
+                                    format!(
                                         "Failed to open token file \"{token_filename}\": {err}"
                                     ),
                                 )?,
