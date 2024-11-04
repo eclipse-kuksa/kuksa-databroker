4f9cb65941dd2f87c350b934636e06b7ab8bc11a
LikhithST <LikhithSadahalliThammegowda@gmail.com>
2024-11-04 16:46:23 +0100

    otel_endpoint as env variable

--- databroker/src/open_telemetry.rs
+++ databroker/src/open_telemetry.rs
@@ -4,6 +4,7 @@ use {
  opentelemetry::sdk::{Resource, trace},
  opentelemetry::trace::TraceError,
  opentelemetry_otlp::WithExportConfig,
+ std::env
 };
 
 #[cfg(feature="otel")]
@@ -13,7 +14,7 @@ pub fn init_trace() -> Result<trace::Tracer, TraceError> {
         .with_exporter(
             opentelemetry_otlp::new_exporter()
                 .tonic()
-                .with_endpoint("http://localhost:4317"),
+                .with_endpoint(env::var("OTEL_ENDPOINT").unwrap_or_else(|_| "http://localhost:4317".to_string())),
         ).with_batch_config(trace::BatchConfig::default()) // to change default of max_queue_size use .with_max_queue_size(8192) or set env OTEL_BSP_MAX_QUEUE_SIZE, by default it is set to 2_048
         .with_trace_config(
             trace::config().with_resource(Resource::new(vec![KeyValue::new(
