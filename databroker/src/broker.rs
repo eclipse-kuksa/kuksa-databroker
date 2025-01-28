e726e246344460c638858cf971f56b5aa3231c5a
Mike Haller <mike.haller@bosch.com>
2025-01-28 16:03:16 +0100

    Fmt

--- databroker/src/broker.rs
+++ databroker/src/broker.rs
@@ -1673,7 +1673,10 @@ impl AuthorizedAccess<'_, '_> {
         let stream = BroadcastStream::new(receiver).filter_map(move |result| match result {
             Ok(message) => Some(message),
             Err(err) => {
-                warn!("Slow subscriber with capacity {} lagged and missed signal updates: {}", channel_capacity, err);
+                warn!(
+                    "Slow subscriber with capacity {} lagged and missed signal updates: {}",
+                    channel_capacity, err
+                );
                 None
             }
         });
