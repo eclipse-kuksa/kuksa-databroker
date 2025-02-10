b9f789c07eccaa547007209d766be37d4a71d8ca
Likhith <146105078+LikhithST@users.noreply.github.com>
2025-02-10 19:07:33 +0530

    otel feature with tracing functionality only (#119)
    Adding OTEL tracing functionality
    
    ---------
    
    Co-authored-by: Lukas Mittag <lukas.mittag@de.bosch.com>

--- databroker/src/lib.rs
+++ databroker/src/lib.rs
@@ -15,6 +15,7 @@ pub mod authorization;
 pub mod broker;
 pub mod glob;
 pub mod grpc;
+pub mod open_telemetry;
 pub mod permissions;
 pub mod query;
 pub mod types;
@@ -28,6 +29,14 @@ use std::fmt::Write;
 use tracing::info;
 use tracing_subscriber::filter::EnvFilter;
 
+#[cfg(feature = "otel")]
+use {
+    open_telemetry::init_trace, opentelemetry::global,
+    opentelemetry::sdk::propagation::TraceContextPropagator,
+    tracing_subscriber::layer::SubscriberExt,
+};
+
+#[cfg(not(feature = "otel"))]
 pub fn init_logging() {
     let mut output = String::from("Init logging from RUST_LOG");
     let filter = EnvFilter::try_from_default_env().unwrap_or_else(|err| {
@@ -38,6 +47,31 @@ pub fn init_logging() {
     tracing_subscriber::fmt::Subscriber::builder()
         .with_env_filter(filter)
         .try_init()
+        .expect("Unable to install global logging subscriber");
+
+    info!("{}", output);
+}
+
+#[cfg(feature = "otel")]
+pub fn init_logging() {
+    let output = String::from("Init logging from RUST_LOG");
+
+    // Set OpenTelemetry trace propagator
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    // Initialize OpenTelemetry tracer
+    let tracer = init_trace().expect("Failed to initialize tracer");
+
+    // telemetry layer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::INFO) // adjust this log level as needed
+        .finish()
+        .with(telemetry); // Add telemetry layer
+
+    // Set the subscriber as the global default for tracing
+    tracing::subscriber::set_global_default(subscriber)
         .expect("Unable to install global logging subscriber");
 
     info!("{}", output);
