adf59fdb53b83cb8220b6a114bd2feb6f79f021c
Rafael RL <rafael.ruiz-lucena@de.bosch.com>
2025-03-17 13:11:14 +0100

    Clippy

--- databroker/src/viss/v2/server.rs
+++ databroker/src/viss/v2/server.rs
@@ -104,14 +104,20 @@ impl Viss for Server {
                 metadata,
             }));
         } else if let Some(Filter::Paths(paths_filter)) = &request.filter {
-            let request_path = request.path.as_ref();
-            if request_path.contains('*') {
+            if request.path.as_ref().contains('*') {
                 return Err(GetErrorResponse {
                     request_id,
                     ts: SystemTime::now().into(),
                     error: Error::NotFoundInvalidPath,
                 });
             }
+            for path in &paths_filter.parameter {
+                let new_path = format!("{}{}", request.path.clone().as_ref(), path);
+                if let Ok(matcher) = Matcher::new(&new_path) {
+                    request_matcher.push((matcher, request.path.clone().into(), false));
+                }
+            }
+        }
 
             let permissions = resolve_permissions(&self.authorization, &request.authorization)
                 .map_err(|error| GetErrorResponse {
